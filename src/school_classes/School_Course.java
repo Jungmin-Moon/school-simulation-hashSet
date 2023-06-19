3b4d9b616ec0523118b61e2e2dd1bfb78555714e
Jungmin Moon <jmmoon96@gmail.com>
2023-06-19 03:32:28 -0400

    added a new class for courses

---
+++ src/school_classes/School_Course.java
@@ -0,0 +1,11 @@
+package school_classes;
+
+import java.util.LinkedList;
+
+import students.Student;
+
+public class School_Course {
+	private String className;
+	private LinkedList<Student> studentsInCourse;
+	
+}
