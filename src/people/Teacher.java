eb137c6da7f99d149e5c05e706f8c03437003218
Jungmin Moon <jmmoon96@gmail.com>
2023-06-20 02:56:18 -0400

    Added getters and setters to teacher along with constructor
    added member variables to School_Course class

--- src/people/Teacher.java
+++ src/people/Teacher.java
@@ -5,4 +5,40 @@ public class Teacher {
 	private String lastName;
 	private String subject;
 	
+	
+	Teacher(String firstName, String lastName, String subject) {
+		this.setFirstName(firstName);
+		this.setLastName(lastName);
+		this.setSubject(subject);
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getSubject() {
+		return subject;
+	}
+
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
 }
--- src/school_classes/School_Course.java
+++ src/school_classes/School_Course.java
@@ -3,9 +3,14 @@ package school_classes;
 import java.util.LinkedList;
 
 import people.Student;
+import people.Teacher;
 
 public class School_Course {
-	private String className;
+	private String courseSubject;
 	private LinkedList<Student> studentsInCourse;
+	private Teacher courseTeacher;
+	private String courseCode;
+	private static int MAX_STUDENTS_PER_CLASS = 40;
+	
 	
 }
