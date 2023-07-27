9325883b9035753be2b8805ef2750cf4910b0603
Jungmin Moon <jmmoon96@gmail.com>
2023-07-27 03:56:30 -0400

    Created constructor for course and getters and setters
    unsure about current look of constructor

--- src/school_classes/School_Course.java
+++ src/school_classes/School_Course.java
@@ -2,6 +2,7 @@ package school_classes;
 
 import java.util.LinkedList;
 
+
 import people.Student;
 import people.Teacher;
 
@@ -10,7 +11,56 @@ public class School_Course {
 	private LinkedList<Student> studentsInCourse;
 	private Teacher courseTeacher;
 	private String courseCode;
+	private int studentCount;
 	private static int MAX_STUDENTS_PER_CLASS = 40;
+	
+	School_Course (String subject, Teacher teacher, String code) {
+		setCourseSubject(subject);
+		setStudentsInCourse(new LinkedList<>());
+		courseTeacher = teacher;
+		setCourseCode(code);
+		setStudentCount(0);
+	}
+
+	public String getCourseSubject() {
+		return courseSubject;
+	}
+
+	public void setCourseSubject(String courseSubject) {
+		this.courseSubject = courseSubject;
+	}
+
+	public LinkedList<Student> getStudentsInCourse() {
+		return studentsInCourse;
+	}
+
+	public void setStudentsInCourse(LinkedList<Student> studentsInCourse) {
+		this.studentsInCourse = studentsInCourse;
+	}
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+
+	public int getStudentCount() {
+		return studentCount;
+	}
+
+	public void setStudentCount(int studentCount) {
+		this.studentCount = studentCount;
+	}
+
+	public static int getMAX_STUDENTS_PER_CLASS() {
+		return MAX_STUDENTS_PER_CLASS;
+	}
+
+	public static void setMAX_STUDENTS_PER_CLASS(int mAX_STUDENTS_PER_CLASS) {
+		MAX_STUDENTS_PER_CLASS = mAX_STUDENTS_PER_CLASS;
+	}
 	
 	
 }
